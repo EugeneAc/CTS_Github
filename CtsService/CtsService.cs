561aeda620345a09eb62901cf93a2064e3efd245
Julia <julia.aniskina@gmail.com>
2018-12-13 10:52:31 +0600

    CtsService extracted from DBSync

---
+++ CtsService/CtsService.cs
@@ -0,0 +1,93 @@
+﻿using CTS_Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace CtsService
+{
+	public partial class CtsService : ServiceBase
+	{
+		private Timer _weightTimer = null;
+		private Timer _warehouseTimer = null;
+		private int _weightTimerInterval = 60000;
+		private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+		public CtsService()
+		{
+			InitializeComponent();
+			this.CanStop = true;
+			this.CanPauseAndContinue = true;
+			this.AutoLog = true;
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			int.TryParse(ConfigurationManager.AppSettings["timerSkipWeight"], out _weightTimerInterval);
+
+			_weightTimer = new Timer {Interval = _weightTimerInterval};
+			_weightTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.WeightTimer_tick);
+			_weightTimer.Enabled = true;
+
+			_warehouseTimer = new Timer {Interval = CalculateTimerInterval()};
+			_warehouseTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.WarehouseTimer_tick);
+			_warehouseTimer.Enabled = true;
+
+			_logger.Trace(string.Format("Service started. Skip weight change interval = {0} minutes, WarehouseTimer will raise in {1} minutes.", (_weightTimerInterval / 60000).ToString(), (_warehouseTimer.Interval / 60000).ToString()));
+		}
+
+		protected override void OnStop()
+		{
+			_weightTimer.Enabled = false;
+			_warehouseTimer.Enabled = false;
+			_logger.Trace("Service Stopped");
+		}
+
+		protected override void OnPause()
+		{
+			_weightTimer.Enabled = false;
+			_warehouseTimer.Enabled = false;
+			_logger.Trace("Service Paused");
+		}
+
+		protected override void OnContinue()
+		{
+			_weightTimer.Enabled = true;
+			_warehouseTimer.Enabled = true;
+			_logger.Trace("Service Continued");
+		}
+
+		private void WeightTimer_tick(object sender, ElapsedEventArgs e)
+		{
+			_weightTimer.Enabled = false;
+
+			SkipWeightsHandler.ManageWeights();
+
+			_weightTimer.Enabled = true;
+		}
+
+		private void WarehouseTimer_tick(object sender, ElapsedEventArgs e)
+		{
+			_warehouseTimer.Enabled = false;
+
+			WarehouseHandler.CalculateWarehouseTransferAutomatic();
+
+			_warehouseTimer.Interval = CalculateTimerInterval();
+			_warehouseTimer.Enabled = true;
+		}
+
+		private double CalculateTimerInterval()
+		{
+			var nextTime = new DateTime(DateTime.Now.AddHours(1).Year, DateTime.Now.AddHours(1).Month, DateTime.Now.AddHours(1).Day, DateTime.Now.AddHours(1).Hour, 10, 0);
+
+			return nextTime.Subtract(DateTime.Now).TotalMilliseconds;
+		}
+	}
+}
--- DBSyncService/DBSyncService/DBSyncService/DBSync.cs
+++ DBSyncService/DBSyncService/DBSyncService/DBSync.cs
@@ -11,10 +11,10 @@ namespace DBSyncService
 	partial class DBSync : ServiceBase
 	{
 		private Timer SyncTimer = null;
-		private Timer WeightTimer = null;
-		private Timer WarehouseTimer = null;
+		//private Timer WeightTimer = null;
+		//private Timer WarehouseTimer = null;
 		private int syncTimerInterval = 60000;
-		private int weightTimerInterval = 60000;
+		//private int weightTimerInterval = 60000;
 		private static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 
 		public DBSync()
@@ -36,8 +36,8 @@ namespace DBSyncService
 				{
 					if (e.Name == "timerDBSynce")
 						syncTimerInterval = System.Int32.Parse(e.Code) * 60000;
-					if (e.Name == "timerSkipWeight")
-						weightTimerInterval = System.Int32.Parse(e.Code) * 60000;
+					//if (e.Name == "timerSkipWeight")
+					//	weightTimerInterval = System.Int32.Parse(e.Code) * 60000;
 				}
 			}
 
@@ -46,17 +46,17 @@ namespace DBSyncService
 			SyncTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.SyncTimer_tick);
 			SyncTimer.Enabled = true;
 
-			WeightTimer = new Timer();
-			WeightTimer.Interval = weightTimerInterval;
-			WeightTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.WeightTimer_tick);
-			WeightTimer.Enabled = true;
+			//WeightTimer = new Timer();
+			//WeightTimer.Interval = weightTimerInterval;
+			//WeightTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.WeightTimer_tick);
+			//WeightTimer.Enabled = true;
 		
-			WarehouseTimer = new Timer();
-			WarehouseTimer.Interval = CalculateTimerInterval();
-			WarehouseTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.WarehouseTimer_tick);
-			WarehouseTimer.Enabled = true;
+			//WarehouseTimer = new Timer();
+			//WarehouseTimer.Interval = CalculateTimerInterval();
+			//WarehouseTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.WarehouseTimer_tick);
+			//WarehouseTimer.Enabled = true;
 
-			_logger.Trace(string.Format("Service started. Timer interval DB Synchronization = {0} minutes, Skip weight change = {1} minutes, WarehouseTimer will raise in {2} minutes.", (syncTimerInterval / 60000).ToString(), (weightTimerInterval / 60000).ToString(), (WarehouseTimer.Interval / 60000).ToString()));
+			_logger.Trace(string.Format("Service started. Timer interval DB Synchronization = {0} minutes", (syncTimerInterval / 60000).ToString()));
 		}
 
 		protected override void OnStop()
@@ -89,31 +89,31 @@ namespace DBSyncService
 			SyncTimer.Enabled = true;
 		}
 
-		private void WeightTimer_tick(object sender, ElapsedEventArgs e)
-		{
-			WeightTimer.Enabled = false;
+		//private void WeightTimer_tick(object sender, ElapsedEventArgs e)
+		//{
+		//	WeightTimer.Enabled = false;
 
-			Task WeightsTask = Task.Factory.StartNew(() => SkipWeightsHandler.ManageWeights());
-			WeightsTask.Wait();
+		//	Task WeightsTask = Task.Factory.StartNew(() => SkipWeightsHandler.ManageWeights());
+		//	WeightsTask.Wait();
 
-			WeightTimer.Enabled = true;
-		}
+		//	WeightTimer.Enabled = true;
+		//}
 
-		private void WarehouseTimer_tick(object sender, ElapsedEventArgs e)
-		{
-			WarehouseTimer.Enabled = false;
+		//private void WarehouseTimer_tick(object sender, ElapsedEventArgs e)
+		//{
+		//	WarehouseTimer.Enabled = false;
 
-			WarehouseHandler.CalculateWarehouseTransferAutomatic();
+		//	WarehouseHandler.CalculateWarehouseTransferAutomatic();
 
-			WarehouseTimer.Interval = CalculateTimerInterval();
-			WarehouseTimer.Enabled = true;
-		}
+		//	WarehouseTimer.Interval = CalculateTimerInterval();
+		//	WarehouseTimer.Enabled = true;
+		//}
 
-		private double CalculateTimerInterval()
-		{
-			var nextTime = new DateTime(DateTime.Now.AddHours(1).Year, DateTime.Now.AddHours(1).Month, DateTime.Now.AddHours(1).Day, DateTime.Now.AddHours(1).Hour, 10, 0);
+		//private double CalculateTimerInterval()
+		//{
+		//	var nextTime = new DateTime(DateTime.Now.AddHours(1).Year, DateTime.Now.AddHours(1).Month, DateTime.Now.AddHours(1).Day, DateTime.Now.AddHours(1).Hour, 10, 0);
 
-			return nextTime.Subtract(DateTime.Now).TotalMilliseconds;
-		}
+		//	return nextTime.Subtract(DateTime.Now).TotalMilliseconds;
+		//}
 	}
 }
